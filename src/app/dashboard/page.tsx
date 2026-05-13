614601a2cf39cfa5a4c3a3f3f879f6bc5aa95222
Myuchii <lea-marie.rebert@epitech.eu>
2026-05-13 08:35:01 +0200

    [ADD] page d'acceuil

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,3 +1,4 @@
+export const dynamic = 'force-dynamic'
 import { PrismaClient } from '@prisma/client'
 import StockTable from '@/components/StockTable'
 import PriceCalculator from '@/components/PriceCalculator'
