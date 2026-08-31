70b2156873de4291df0f30c492bad46cec8e7fdc
Myuchii <lea-marie.rebert@epitech.eu>
2026-08-31 09:15:18 +0200

    [FIX] recuperé commande paypal attente

--- src/app/_actions/prestashop-actions.ts
+++ src/app/_actions/prestashop-actions.ts
@@ -114,7 +114,7 @@ export async function syncPrestashopOrders() {
     const data = JSON.parse(responseText)
     const rawOrders = data.orders || []
 
-    const targetStatuses = ['1','2', '3', '4', '5', '10','11', '12', '14']
+    const targetStatuses = ['1','2', '3', '4', '5', '10','11', '15', '19', '21']
     const psOrders = rawOrders.filter((order: any) => targetStatuses.includes(String(order.current_state)))
 
     let importedCount = 0
